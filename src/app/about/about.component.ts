c344731d87b3964492d269eb06bc781c2deae2e0
vivek vishwakarma <vivekiam231@gmail.com>
2023-09-14 14:37:02 +0530

    create a new project

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
+})
+export class AboutComponent {
+first:Number=50;
+second:String="kijvg";
+
+abc()
+{
+  alert("welcome to angular with typescript");
+}
+}
